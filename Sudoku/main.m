ab991f8a9c9b0eb801aa9140c9394477962123dc
Ruan Pingcheng <ruanpingcheng@gmail.com>
2013-08-03 21:34:05 -0400

    Repo Initialization

---
+++ Sudoku/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Sudoku
+//
+//  Created by Ruan Pingcheng on 13-8-3.
+//  Copyright (c) 2013年 Ruan Pingcheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
